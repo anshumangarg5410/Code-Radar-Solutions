00617856e3d2f9ea1fc51a67f9308568dee1761d
Anshuman Garg <anshumangarg5410@gmail.com>
2025-01-21 09:49:38 +0530

    Solution to Input and Print a String

--- Input and Print a String@@6788d5012db25efec946f93c/code.c
+++ Input and Print a String@@6788d5012db25efec946f93c/code.c
@@ -6,7 +6,6 @@ int main() {
     char *mystring = (char *)malloc((1024) * sizeof(char));
     scanf("%[^\n]", mystring);
     mystring = realloc(mystring, strlen(mystring) + 1);
-    printf("%s", mystring);
-
+    printf("You entered %s", mystring);
     return 0;
 }

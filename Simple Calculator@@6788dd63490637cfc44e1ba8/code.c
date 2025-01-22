d7b9b211e755edd8d817fcdb6a0aac8f572acfdb
Anshuman Garg <anshumangarg5410@gmail.com>
2025-01-22 12:48:40 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,22 +1,54 @@
+// #include <stdio.h>
+
+// int main(){
+//     int a, b;
+//     char op;
+//     scanf("%d %d %c", &a, &b, &op);
+
+//     int result = a;
+//     if (op == '+'){
+//         printf("%d", a + b);
+//     }
+//     else if(op == '-'){
+//         printf("%d", a - b);
+//     }
+//     else if(op == '*'){
+//         printf("%d", a*b);
+//     }
+//     else if(op == '/'){
+//         printf("%d", a/b);
+//     }
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main(){
     int a, b;
     char op;
+
+    // Space added in scanf to prevent newline issues with %c
     scanf("%d %d %c", &a, &b, &op);
 
-    int result = a;
     if (op == '+'){
-        printf("%d", a + b);
+        printf("%d\n", a + b);
     }
     else if(op == '-'){
-        printf("%d", a - b);
+        printf("%d\n", a - b);
     }
     else if(op == '*'){
-        printf("%d", a*b);
+        printf("%d\n", a * b);
     }
     else if(op == '/'){
-        printf("%d", a/b);
+        if (b == 0) { // Check for division by zero
+            printf("Error: Division by zero is not allowed.\n");
+        } else {
+            printf("%d\n", a / b);
+        }
     }
+    else {
+        printf("Error: Invalid operator.\n"); // Handle invalid operators
+    }
+
     return 0;
 }

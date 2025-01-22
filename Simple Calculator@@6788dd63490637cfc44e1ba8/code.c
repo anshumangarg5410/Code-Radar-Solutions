0696b575b3d7af42eb4acd166bb335736f4fbe3b
Anshuman Garg <anshumangarg5410@gmail.com>
2025-01-23 00:06:08 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,10 +3,10 @@
 int main(){
     int a,b;
     char c;
-    if (c == "+") printf("%d", a + b);
-    else if (c == "-") printf("%d", a - b);
-    else if (c == "*") printf("%d", a * b);
-    else if (c == "/") {
+    if (c == '+') printf("%d", a + b);
+    else if (c == '-') printf("%d", a - b);
+    else if (c == '*') printf("%d", a * b);
+    else if (c == '/') {
         if (b == 0){
             printf("error");
         }

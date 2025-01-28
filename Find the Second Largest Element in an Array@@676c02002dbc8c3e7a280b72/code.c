816f58d38d7ffb7aafd83c3473ca8b790e9aa298
Anshuman Garg <anshumangarg5410@gmail.com>
2025-01-28 23:06:44 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -1,32 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h> // for INT_MIN
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<limits.h>
-
-int max_ele(int myarray[],int a){
-    int maxx = INT_MIN;
-    for(int i =0; i<a; i++){
-        if (myarray[i] >= maxx){
+int max_ele(int myarray[], int a) {
+    int maxx = INT_MIN; // safer initialization
+    for(int i = 0; i < a; i++) {
+        if (myarray[i] > maxx) { // strictly greater for max value
             maxx = myarray[i];
         }
     }
     return maxx;
 }
 
-
-int main(){
+int main() {
     int a;
     scanf("%d", &a);
-    int *myarray = (int *)malloc((a)*(sizeof(int)));
-    for (int i =0; i<a; i++){
+    int *myarray = (int *)malloc(a * sizeof(int));
+    
+    for (int i = 0; i < a; i++) {
         scanf("%d", &myarray[i]);
     }
+    
     int maxele = max_ele(myarray, a);
-    for(int i=0; i<a; i++){
-        if(myarray[i] == maxele){
+    
+    // Set the maximum element(s) to 0
+    for (int i = 0; i < a; i++) {
+        if (myarray[i] == maxele) {
             myarray[i] = 0;
         }
     }
+    
+    // Find the second largest element
     int secondlarg = max_ele(myarray, a);
     printf("%d", secondlarg);
+    
+    free(myarray); // Don't forget to free the allocated memory
+    return 0;
 }

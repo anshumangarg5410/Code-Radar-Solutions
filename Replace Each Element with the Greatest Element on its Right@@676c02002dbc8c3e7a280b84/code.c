a9a5b688e06a9a312885e38b67068a194ec4f7a0
Anshuman Garg <anshumangarg5410@gmail.com>
2025-02-05 23:01:24 +0530

    Solution to Replace Each Element with the Greatest Element on its Right

---
+++ Replace Each Element with the Greatest Element on its Right@@676c02002dbc8c3e7a280b84/code.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+int maxele(myarray){
+    int max;
+    max = myarray[0];
+    for (int i =0; i<a; i++){
+        if(myarray[i] >= max) max = myarray[i];
+    }
+    return max;
+}
+
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    int *vector = malloc((a) * sizeof(int));
+    int *finall = malloc((a) * sizeof(int));
+    for (int i =0; i<a; i++){
+        scanf("%d", &vector[i]);
+    }
+    for(int i =0; i<a; i++){
+        if (i == a-1) finall[i] = -1;
+        else {
+            vector[i] = 0;
+            finall[i] = maxele(myarray);
+        }
+
+    }
+    for (int j =0; j<a; j++){
+        printf("%d ", finall[j]);
+    }
+    return 0;
+}

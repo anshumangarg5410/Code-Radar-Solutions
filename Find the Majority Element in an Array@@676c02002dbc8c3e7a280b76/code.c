0961299de5ec20b5b00782fd714e8a27c4e186f2
Anshuman Garg <anshumangarg5410@gmail.com>
2025-03-01 16:56:16 +0530

    Solution to Find the Majority Element in an Array

--- Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
+++ Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
@@ -1,41 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<limit.h>
 
 int main(){
     int a;
     scanf("%d", &a);
-    int *myarray = (int *)malloc((a) * sizeof(int));
-    int *countaray = (int *)malloc((a) * sizeof(int));
-    int maxcount = 0;
-    int maxele = 0;
-    int specnt;
+    int *myarray = malloc(a * sizeof(int));
     for (int i =0; i<a; i++){
         scanf("%d", &myarray[i]);
     }
+    int j =0;
+    int counttemp = 0; int target = 0; int countfinal = 0;
     for (int i =0; i<a; i++){
-        countaray[i] = 1;
-    }
-    for (int i = 0; i<a; i++){
-        specnt = 0;
-        for (int j =0; j<a; j++){
-            if (myarray[i] == myarray[j]) specnt++;
+        if (myarray[i] == myarray[j] && i!=j){
+            counttemp++; 
         }
-        countaray[i] = specnt;
-        if (specnt > maxcount) {
-            maxcount = specnt;
-            maxele = myarray[i]; //make it this independeny
+        if (i == a-1 && j != a-1) {
+            if (counttemp > countfinal) {
+                countfinal = counttemp;
+                target = j;
+            }
+            i = 0;
         }
+        j++;
     }
-    // for(int i=0; i<a; i++){
-    //     printf("%d", countaray[i]);
-    // }
-    int repcnt = 0;
-    for (int i =0; i<a; i++){
-        if (countaray[i] == maxcount){
-            repcnt++;
-        }
-    
-    }
-    if (repcnt ==  maxcount) printf("%d", maxele);
-    else printf("%d", -1);
+    printf("%d", myarray[target]);
 }

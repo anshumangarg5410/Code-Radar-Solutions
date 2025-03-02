4e5bae6e272574ba634f1396d9ff33b906e622f9
Anshuman Garg <anshumangarg5410@gmail.com>
2025-03-02 12:04:21 +0530

    Solution to Find the Leaders in an Array

--- Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
+++ Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
@@ -4,29 +4,14 @@
 int main(){
     int a;
     scanf("%d", &a);
-    int *myvec = malloc((a) * sizeof(int));
-    for(int i =0;i<a;i++){
-        scanf("%d", &myvec[i]);
+    int *myarray = malloc(a * sizeof(int));
+    for(int i =0; i<a; i++){
+        scanf("%d", &myarray[i]);
     }
-    int i = 0; int flag = 0;
-    for(int j =i+1; j<a+1; j++){
-        if (myvec[i] < myvec[j]) {
-            flag++;
-        } else {
-            flag = 0;
-            i++;
-            j=0;
+    for (int j =0; j<a; j++){
+        if (((j == 0 && myarray[j+1]) < myarray[j]) || (j == a-1) && (myarray[j] > myarray[j-1])) printf("%d", myarray[j]);
+        if (j != 0 || j != a-1) {
+            if (myarray[j] > myarray[j-1] && myarray[j] > myarray[j+1]) printf("%d", myarray[j]);
         }
-        if (j == a-1) j = 0;
-        
-        if(i == a-1) {
-            break;
-        }
-
-        if (flag == a - i - 1) {
-            printf("%d", myvec[i]);
-        }
-
-        
     }
 }

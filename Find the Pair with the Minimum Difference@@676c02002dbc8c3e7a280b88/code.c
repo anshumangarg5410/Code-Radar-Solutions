2ea1d290173f5859f22b9ead7971430182ed200a
Anshuman Garg <anshumangarg5410@gmail.com>
2025-03-02 13:03:47 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -1,34 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
 
-int main() {
+int main()[
     int a;
     scanf("%d", &a);
-    if (a == 1) {
-        printf("-1");
-        return 0;
+    int *myarray = malloc(a * sizeof(int));
+    for(it i = 0; i<a; i++){
+        scanf("%d", &myarray[i]);
     }
-    int *vector = malloc((a) * sizeof(int));
-    for(int i =0; i<a; i++){
-        scanf("%d", &vector[i]);
-    }
-    int ele1 = INT_MAX;
-    int ele2 = INT_MAX;
-    int minn = INT_MAX;
-    for(int i =0; i<a-1; i++){
-        for (int j=0; j<a; j++){
-            if (abs(vector[i] - vector[j]) < minn && i!=j) {
-                if (ele1 > vector[i] && ele2 > vector[j]){               
-                    minn = abs(vector[i] - vector[j]);
-                    ele1 = vector[i];
-                    ele2 = vector[j];
-                }
-
-            };
+    int diff = INT_MAX; int ele1; int ele2; int j = 0;
+    for(int i =0; i<=a; i++){
+        if (abs(myarray[j] - myarray[i]) < diff) {
+            ele1 = myarray[j]; ele2 = myarray[i]; diff = abs(myarray[j] - myarray[i]);
         }
+        if (i = a-1) {
+            i = -1;
+            j++;
+        }
+        if (j == a-1) break;
     }
+
     if (ele1 > ele2) printf("%d %d", ele2, ele1);
     else printf("%d %d", ele1, ele2);
-    return 0;
-}
+
+]

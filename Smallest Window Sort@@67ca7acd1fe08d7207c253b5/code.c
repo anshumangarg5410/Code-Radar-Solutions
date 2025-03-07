28a9d12d9cadb1100f2dc7a8f0cb669a63b49493
Anshuman Garg <anshumangarg5410@gmail.com>
2025-03-07 21:55:16 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -3,13 +3,13 @@
 int findUnsortedSubarray(int arr[], int size){
     int first, last;
     for(int i =0; i<size-1; i++){
-        if (!((arr[i-1] < arr[i] && arr[i] < arr[i+1]) && (i != 0 && i != size-1)) || ((arr[i] > arr[i + 1]) && i == 0) || ((arr[i] < arr[i-1]))) {
+        if (!((arr[i-1] < arr[i] && arr[i] < arr[i+1]) && (i != 0 && i != size-1)) || (!((arr[i] > arr[i + 1]) && i == 0) || ((arr[i] < arr[i-1])))) {
             first = arr[i];
             break;
         }
     } 
     for(int i =size-1; i>=0; i--){
-        if (!((arr[i-1] < arr[i] && arr[i] < arr[i+1]) && (i != 0 && i != size-1)) || ((arr[i] > arr[i + 1]) && i == 0) || ((arr[i] < arr[i-1]))) {
+        if (!((arr[i-1] < arr[i] && arr[i] < arr[i+1]) && (i != 0 && i != size-1)) || (!((arr[i] > arr[i + 1]) && i == 0) || ((arr[i] < arr[i-1])))) {
             last = arr[i];
             break;
         }

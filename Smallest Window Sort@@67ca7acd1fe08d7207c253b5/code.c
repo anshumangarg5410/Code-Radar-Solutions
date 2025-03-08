d624735867fab82bba342e32074a255fff88a762
Anshuman Garg <anshumangarg5410@gmail.com>
2025-03-08 11:49:04 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
 
-int findUnsortedSubarray(int arr[], int size){
+int findsortedSubarray(int arr[], int size){
     int first, last;
-    for(int i =0; i<size-1; i++){
-        if ((i == 0) && (arr[0] > arr[1])){
-            first = arr[i];
-            break;
+
+    for (int i =0; i<size-1; i++){
+        if (i == 0){
+            if (arr[i] > arr[i+1]){
+                first = arr[i];
+                break;
             }
-        if (((arr[i-1] > arr[i] && arr[i] > arr[i+1]) && (i != 0 && i != size-1))) {
-            first = arr[i];
-            break;
         }
+        else {
+            if (arr[i] < arr[i-1] || arr[i] > arr[i+1]) {
+                first = arr[i];
+                break;
+            }
         }
+    }
 
-    for(int i =size-1; i>0; i--){
-
-        if ((i == size) && (arr[size] < arr[size-1])) {
-            last = arr[i];
-            break;
+    for (int i =size-1; i>0; i--){
+        if (i == size-1){
+            if (arr[i] < arr[i-1]){
+                last = arr[i];
+                break;
+            }
         }
-
-        if (((arr[i-1] > arr[i] && arr[i] > arr[i+1]) && (i != 0 && i != size-1))) {
-            last = arr[i];
-            break;
+        else {
+            if (arr[i] < arr[i-1] || arr[i] > arr[i+1]) {
+                last = arr[i];
+                break;
+            }
         }
-    } 
-    printf("\n%d %d\n", first, last);
-
+    }
+    printf("%d %d", first, last);
 }
-

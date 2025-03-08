289266c9b66ae5ff60f3abe0ac4c8cb3229b5cd0
Anshuman Garg <anshumangarg5410@gmail.com>
2025-03-08 12:13:19 +0530

    Solution to Selection Sort for Integers

---
+++ Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int selectionsorter(int arr[], int size){
+    for (int i = 0; i<size; i++){
+        int minele = 0;
+        for (int j = i+1; j<size; i++){
+            if (arr[j] <= minele) minele = j;
+        }
+        int temp = arr[i];
+        arr[i] = arr[minele];
+        arr[minele] = temp;
+    }
+    return arr;
+}

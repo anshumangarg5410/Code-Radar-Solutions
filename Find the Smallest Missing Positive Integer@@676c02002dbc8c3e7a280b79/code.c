1741f13fc6724e334f935ac8f8a8c80e3440bfeb
Anshuman Garg <anshumangarg5410@gmail.com>
2025-03-12 11:29:04 +0530

    Solution to Find the Smallest Missing Positive Integer

--- Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
+++ Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
@@ -2,41 +2,20 @@
 #include<stdlib.h>
 #include<limits.h>
 
-int maxele(int myarray[], int size){
-    int maxx = 0;
-    for (int i =0; i<size; i++){
-        if (myarray[i] > maxx) maxx = myarray[i];
-    }
-    return maxx;
-}
-
-int minele(int myarray[], int size){
-    int minn = INT_MAX;
-    for (int i =0; i<size; i++){
-        if (myarray[i] < minn) minn = myarray[i];
-    }
-    return minn;
-}
-
 int main(){
     int a;
     scanf("%d", &a);
     int *myarray = malloc(a * sizeof(int));
-    for (int i =0; i<a; i++){
+    for(int i =0; i<a; i++){
         scanf("%d", &myarray[i]);
     }
-    int j = 1; int element; int condition = 0;
-    for (int i =0; i<=a; i++){
-        if (j == a) break;
-        if (myarray[i] != minele(myarray, a) + j)  {
-            condition++; printf("condition updated at i: %d j: %d\n", i, j);
-        }
-        if (i == a-1){
-            if (condition == a) element = myarray[j]; printf("stopped at i: %d j: %d and condition: %d\n", i, j, condition);
-            condition = 0;
-            i = -1;
-            j++;
+    int e = 0;
+    for(int i = 0;i<a;i++) {
+        if(myarr[i] == 1) {
+            e++;
         }
     }
-    printf("%d", element);
+    if(e == 0) {
+        printf("1");
+    }
 }

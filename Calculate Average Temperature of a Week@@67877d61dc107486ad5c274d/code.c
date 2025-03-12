9b9ba8c8615583c791b4918c6577802ea48e1864
Anshuman Garg <anshumangarg5410@gmail.com>
2025-03-12 19:22:24 +0530

    Solution to Calculate Average Temperature of a Week

--- Calculate Average Temperature of a Week@@67877d61dc107486ad5c274d/code.c
+++ Calculate Average Temperature of a Week@@67877d61dc107486ad5c274d/code.c
@@ -1,6 +1,33 @@
+// #include<stdio.h>
+// #include<stdlib.h>
+// #include<string.h>
+
+// typedef struct temp {
+//     char name[100];
+//     float temp;
+// } temp;
+
+// int main(){
+//     int a;
+//     scanf("%d", &a);
+//     // temp s[a];
+//     temp* s = (temp*)malloc(a * sizeof(temp));
+//     for(int i = 0; i<a; i++){
+//         scanf("%s %f", &s[i].name, &s[i].temp);
+//     }
+//     float summ = 0.0;
+//     for (int i =0; i<a; i++){
+//         summ += s[i].temp;
+//     }
+//     float averagee = (summ)/a;
+//     printf("Average Tempreature: %.2f", averagee);
+
+// }
+
+
+
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 
 typedef struct temp {
     char name[100];
@@ -10,16 +37,21 @@ typedef struct temp {
 int main(){
     int a;
     scanf("%d", &a);
-    // temp s[a];
-    temp* s = (temp*)malloc(a * sizeof(temp));
-    for(int i = 0; i<a; i++){
-        scanf("%s %f", &s[i].name, &s[i].temp);
+
+    temp* s = (temp*)malloc(a * sizeof(temp)); // dynamic allocation
+
+    for(int i = 0; i < a; i++){
+        scanf("%s %f", s[i].name, &s[i].temp);
     }
+
     float summ = 0.0;
-    for (int i =0; i<a; i++){
+    for (int i = 0; i < a; i++){
         summ += s[i].temp;
     }
-    float averagee = (summ)/a;
-    printf("Average Tempreature: %.2f", averagee);
 
+    float averagee = summ / a;
+    printf("Average Temperature: %.2f\n", averagee);
+
+    free(s); // free memory
+    return 0;
 }

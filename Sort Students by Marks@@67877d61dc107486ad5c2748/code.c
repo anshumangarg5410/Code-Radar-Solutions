6b5c0afcd1f613e66b3b9fea6893fc3431a24c9e
Anshuman Garg <anshumangarg5410@gmail.com>
2025-03-12 18:33:06 +0530

    Solution to Sort Students by Marks

--- Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -1,7 +1,45 @@
    
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+// #include<stdio.h>
+// #include<stdlib.h>
+// #include<string.h>
+
+// typedef struct student {
+//     int rollnumber;
+//     char name[100];
+//     float marks;
+// } student;
+
+// void bubblesort(struct student s[], int a){
+//     for(int i =0; i<a; i++){
+//         for(int j = 0; j<a-1; j++){
+//             if (s[j].marks > s[j+1].marks) {
+//                 student temp = s[j];
+//                 s[j] = s[j + 1];
+//                 s[j + 1] = temp;
+//             }
+//         }
+//     }
+// }
+
+// int main(){
+//     int a;
+//     scanf("%d", &a);
+//     student s[a];
+//     for(int i = 0; i<a; i++){
+//         scanf("%d %s %f", &s[i].rollnumber, &s[i].name, &s[i].marks);
+//     }
+//     bubblesort(s, a);
+
+//     for (int i = 0; i<a; i++){
+//         printf("Roll Number: %d, Name: %s, Marks: %.2f", s[i].rollnumber, s[i].name, s[i].marks);
+//         printf("\n");
+//     }
+
+// }
+   
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct student {
     int rollnumber;
@@ -22,19 +60,24 @@ void bubblesort(student s[], int a) {
     }
 }
 
-int main(){
+int main() {
     int a;
+    printf("Enter number of students: ");
     scanf("%d", &a);
+
     student s[a];
-    for(int i = 0; i<a; i++){
-        scanf("%d %s %f", &s[i].rollnumber, &s[i].name, &s[i].marks);
+
+    printf("Enter roll number, name and marks of each student:\n");
+    for (int i = 0; i < a; i++) {
+        scanf("%d %s %f", &s[i].rollnumber, s[i].name, &s[i].marks);
     }
+
     bubblesort(s, a);
 
-    for (int i = 0; i<a; i++){
-        printf("Roll Number: %d, Name: %s, Marks: %.2f", s[i].rollnumber, s[i].name, s[i].marks);
-        printf("\n");
+    printf("\nSorted Students (by Marks):\n");
+    for (int i = 0; i < a; i++) {
+        printf("Roll Number: %d, Name: %s, Marks: %.2f\n", s[i].rollnumber, s[i].name, s[i].marks);
     }
 
+    return 0;
 }
-   

3fa09917cd3b7ed7776c4f2160b4d778875e903e
Anshuman Garg <anshumangarg5410@gmail.com>
2025-03-13 13:38:48 +0530

    Solution to Check if a String is a Pangram

--- Check if a String is a Pangram@@67877c59ddaeab38c8ad25b2/code.c
+++ Check if a String is a Pangram@@67877c59ddaeab38c8ad25b2/code.c
@@ -1,36 +1,3 @@
-// #include<stdio.h>
-// #include<stdlib.h>
-// #include<string.h>
-
-// int unique(char target, int a){
-//     int count = 0;
-//     char alphabet[54];
-//     for (int i = 0; i < 26; i++) {
-//         alphabet[i] = 'a' + i;       
-//         alphabet[i + 26] = 'A' + i;  
-//     }
-//     alphabet[53] = ' '; 
-//     for(int i = 0;i<a; i++){
-//         if (alphabet[i] == target) count++;
-//     }
-//     if (count >= 1) return 1;
-//     else return 0;
-// }
-
-// int main(){
-//     char myar[100];
-//     fgets(myar, 100, stdin);
-//     myar[strcspn(myar, "\n")] = '\0';
-//     int len = strlen(myar);
-//     for(int i =0; i<len; i++){
-//         if (!unique(myar[i], len)) {
-//             printf("No");
-//             return 0;
-//         }
-//     }
-//     printf("Yes");
-// }
-
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
@@ -38,29 +5,22 @@
 int is_present(char c, char input[]) {
     int len = strlen(input);
     for (int i = 0; i < len; i++) {
-        if (tolower(input[i]) == tolower(c)) {
-            return 1;  // found the letter
-        }
+        if (tolower(input[i]) == tolower(c)) return 1;
     }
-    return 0;  // not found
+    return 0; 
 }
 
 int main() {
     char myar[1000];
     fgets(myar, 1000, stdin);
-    myar[strcspn(myar, "\n")] = '\0';  // Remove newline
-
+    myar[strcspn(myar, "\n")] = '\0'; 
     char alphabet[26];
-    for (int i = 0; i < 26; i++) {
-        alphabet[i] = 'a' + i;
-    }
-
+    for (int i = 0; i < 26; i++) alphabet[i] = 'a' + i;
     for (int i = 0; i < 26; i++) {
         if (!is_present(alphabet[i], myar)) {
             printf("No\n");
             return 0;
         }
     }
-
-    printf("Yess\n");
+    printf("Yes\n");
 }

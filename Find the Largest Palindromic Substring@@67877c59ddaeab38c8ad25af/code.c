338943ec1a2b899e27644f918d3094abbed70912
Anshuman Garg <anshumangarg5410@gmail.com>
2025-03-13 12:16:28 +0530

    Solution to Find the Largest Palindromic Substring

--- Find the Largest Palindromic Substring@@67877c59ddaeab38c8ad25af/code.c
+++ Find the Largest Palindromic Substring@@67877c59ddaeab38c8ad25af/code.c
@@ -21,15 +21,12 @@ int main(){
         temp[0] = mystr[i];
         for(int  j =i+1; j<n; j++){
             temp[k] = mystr[j];
-            if (temp[k] != mystr[j]){
-                if (palindrome(temp, k+1)) {
-                    for(int o =0; o<k+1; o++){
-                        printf("%c", temp[o]);
-                    }
-                    return 0;
+            if (palindrome(temp, k+1)) {
+                for(int o =0; o<k+1; o++){
+                    printf("%c", temp[o]);
                 }
+                return 0;
             }
-           
             k++;
         }
     }

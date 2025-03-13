b2799c6290a3e43f06d4d527f04c554f441f97c7
Anshuman Garg <anshumangarg5410@gmail.com>
2025-03-13 13:28:59 +0530

    Solution to Check if a String is a Pangram

--- Check if a String is a Pangram@@67877c59ddaeab38c8ad25b2/code.c
+++ Check if a String is a Pangram@@67877c59ddaeab38c8ad25b2/code.c
@@ -4,7 +4,7 @@
 
 int unique(char target, int a){
     int count = 0;
-    char alpha[52];
+    char alphabet[52];
     for (int i = 0; i < 26; i++) {
         alphabet[i] = 'a' + i;       
         alphabet[i + 26] = 'A' + i;  

5a3e867fd8ed8d97b77ef060bab126c10ab1b804
Anshuman Garg <anshumangarg5410@gmail.com>
2025-03-13 14:31:12 +0530

    Solution to Selection Sort for Strings

--- Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
+++ Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
@@ -1,34 +1,34 @@
 #include<stdio.h>
 #include<string.h>
 
-// void selectionSort(char arr[][100], int size) {
-//     for (int i = 0; i < size-1; i++) {
-//         int minele = i; 
-//         for (int j=i+1; j<size; j++) {
-//             if (strcmp(arr[j], arr[minele]) < 0) { 
-//                 minele = j;
-//             }
-//         }
-//         char temp[100];
-//         strcpy(temp, arr[i]);
-//         strcpy(arr[i], arr[minele]);
-//         strcpy(arr[minele], temp);
-//     }
-// }
-
-void selectionSort(char myarr[][100], int a) {
-    for(int i = 0;i<a-1;i++) {
-        for(int j = 0;j<a-1;j++) {
-            int y = strcmp(myarr[j],myarr[j+1]);
-            if(y > 0) {
-                char temp[100];
-                strcpy(temp,myarr[j+1]);
-                strcpy(myarr[j+1],myarr[j]);
-                strcpy(myarr[j],temp);
+void selectionSort(char arr[][100], int size) {
+    for (int i = 0; i < size-1; i++) {
+        int minele = i; 
+        for (int j=i+1; j<size; j++) {
+            if (strcmp(arr[j], arr[minele]) < 0) { 
+                minele = j;
             }
         }
+        char temp[100];
+        strcpy(temp, arr[i]);
+        strcpy(arr[i], arr[minele]);
+        strcpy(arr[minele], temp);
     }
 }
+
+// void selectionSort(char myarr[][100], int a) {
+//     for(int i = 0;i<a-1;i++) {
+//         for(int j = 0;j<a-1;j++) {
+//             int y = strcmp(myarr[j],myarr[j+1]);
+//             if(y > 0) {
+//                 char temp[100];
+//                 strcpy(temp,myarr[j+1]);
+//                 strcpy(myarr[j+1],myarr[j]);
+//                 strcpy(myarr[j],temp);
+//             }
+//         }
+//     }
+// }
 
 
 

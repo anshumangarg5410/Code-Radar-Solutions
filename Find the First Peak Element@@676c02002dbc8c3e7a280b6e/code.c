ae2fc9872e265b0b1b891ce41b71201b4677ff6e
Anshuman Garg <anshumangarg5410@gmail.com>
2025-03-14 16:28:42 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -1,58 +1,25 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// int main(){
-//     int a;
-//     scanf("%d", &a);
-//     int *myarray = (int *)malloc((a+1) * sizeof(int));
-//     for (int i = 0; i<a+1; i++){
-//         scanf("%d", &myarray[i]);
-//     }
-//     int condition = 0;
-//     int result;
-//     for (int i =1; i<a;i++){
-//         if (myarray[i] > myarray[i-1] && myarray[i] > myarray[i+1]){
-//             condition = 1;
-//             result = myarray[i];
-//             break;
-//         }
-//     }
-//     if (condition){
-//         printf("%d", result);
-//     }
-//     else printf("%d", -1);
-// }
-
-
-#include<stdio.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 int main(){
-
-    int n;    
-
-    scanf("%d", &n);
-
-    int lst[n];
-
-    for (int i = 0; i<n; i++){
-
-        scanf("%d", &lst[i]);
-
+    int a;
+    scanf("%d", &a);
+    int *myarray = (int *)malloc((a+1) * sizeof(int));
+    for (int i = 0; i<a+1; i++){
+        scanf("%d", &myarray[i]);
     }
-    lst[n] = 0;
-    for (int i = 1; i<n;i++){
-
-        if (lst[i]>lst[i-1] && lst[i]>lst[i+1]){
-
-            printf("%d", lst[i]);
-
-            return 0;
-
+    int condition = 0;
+    int result;
+    for (int i =1; i<a;i++){
+        if (myarray[i] > myarray[i-1] && myarray[i] > myarray[i+1]){
+            condition = 1;
+            result = myarray[i];
+            break;
         }
-
     }
-
-    printf("-1");
-
-    return 0;
-
+    if (condition){
+        printf("%d", result);
+    }
+    else printf("%d", -1);
 }
+
+
